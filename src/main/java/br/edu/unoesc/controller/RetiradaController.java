f293996b64753f930280bdfc8be3b726fd5f8df8
Eduardo <eduardosantin38@gmail.com>
2019-09-21 16:08:19 -0300

    salvando retirada dos livros

--- src/main/java/br/edu/unoesc/controller/RetiradaController.java
+++ src/main/java/br/edu/unoesc/controller/RetiradaController.java
@@ -2,6 +2,7 @@ package br.edu.unoesc.controller;
 
 import br.edu.unoesc.model.Retirada;
 import br.edu.unoesc.repository.LivroRepository;
+import br.edu.unoesc.repository.PessoaRepository;
 import br.edu.unoesc.service.LivroService;
 import br.edu.unoesc.service.RetiradaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class RetiradaController {
@@ -19,16 +22,25 @@ public class RetiradaController {
     @Autowired
     private LivroRepository livroRepository;
 
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public String caregar(Model model){
+        model.addAttribute("list", livroRepository.findAll());
+        model.addAttribute("listPessoa", pessoaRepository.findAll());
+        return "retirada/cadastrar";
+    }
+
     @GetMapping("/retirada/cadastro")
     public String cadastro(Model model){
-        model.addAttribute("list", livroRepository.findAll());
-        return "retirada/cadastrar";
+        return caregar(model);
     }
 
     @PostMapping("/retirada/cadastro")
     public String cadastro(Model model, Retirada retirada){
+        retirada.setData(LocalDate.now());
         retiradaService.salvar(retirada);
-        return "retirada/cadastrar";
+        return caregar(model);
     }
 
 }
