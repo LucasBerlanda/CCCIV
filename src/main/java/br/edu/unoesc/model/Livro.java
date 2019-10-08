b5224ccda9e0b865f5d0080386dd94a3df0c868e
Lucas Berlanda <lucasberlanda02@gmail.com>
2019-10-07 23:41:40 -0300

    rodape e alguns ajustes

--- src/main/java/br/edu/unoesc/model/Livro.java
+++ src/main/java/br/edu/unoesc/model/Livro.java
@@ -1,10 +1,7 @@
 package br.edu.unoesc.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Entity
